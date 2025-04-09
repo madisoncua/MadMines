3874b6e8e8317cfb54279a048fefa27ea6dd0bf3
Madison Cua <madison.cua@gmail.com>
2025-04-09 12:15:32 -0500

    Add files via upload

---
+++ Machine.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class machine{
+    //global variables needed:
+    friend class Lab9HMain
+    private:
+    uint8_t input;
+    uint8_t state;
+
+
+    public:
+    updateSmelter(uint8_t);
+    updateRefiner(uint8_t);
+    updateRock(uint8_t);
+    updateAnvil(uint8_t);
+    updateTurnInArea(uint8_t);
+}
---
+++ Player.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Player.h"
+
+Player::Player(int x, int y){
+    xPos = x;
+    yPos = y;
+}
+
+Player::Player(){
+    xPos = 500;
+    yPos = 70;
+}
+
+void Player::moveUp(){
+    switch
+    yPos+=2;
+}
+void Player::moveDown(){
+    yPos-=2;
+}
+void Player::moveLeft(){
+    xPos-=2;
+}
+void Player::moveRight(){
+    xPos+=2;
+}
