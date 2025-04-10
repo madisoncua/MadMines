31ee9774cbbfedc841650c1bd169a5ed305ddbb4
Madison Cua <madison.cua@gmail.com>
2025-04-10 15:28:50 -0500

    Add files via upload

--- Player.cpp
+++ Player.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include "Player.h"
+// #include <iostream>
+// #include "Player.h"
 
-Player::Player(int x, int y){
-    xPos = x;
-    yPos = y;
-}
+// Player::Player(int x, int y){
+//     xPos = x;
+//     yPos = y;
+// }
 
-Player::Player(){
-    xPos = 56;
-    yPos = 70;
-}
+// Player::Player(){
+//     xPos = 56;
+//     yPos = 70;
+// }
 
-void Player::moveUp(){
-    switch
-    yPos+=2;
-}
-void Player::moveDown(){
-    yPos-=2;
-}
-void Player::moveLeft(){
-    xPos-=2;
-}
-void Player::moveRight(){
-    xPos+=2;
-}
+// void Player::moveUp(){
+//     switch
+//     yPos+=2;
+// }
+// void Player::moveDown(){
+//     yPos-=2;
+// }
+// void Player::moveLeft(){
+//     xPos-=2;
+// }
+// void Player::moveRight(){
+//     xPos+=2;
+// }
--- Player.h
+++ Player.h
@@ -1,18 +1,18 @@
-#include <iostream>
-using namespace std;
-class Player{
-    friend class machine; //machines can access
-    friend class Lab9HMain;
-    private:
-    int possession;
-    int8_t xPos;
-    int8_t yPos;
+// #include <iostream>
+// using namespace std;
+// class Player{
+//     friend class machine; //machines can access
+//     friend class Lab9HMain;
+//     private:
+//     int possession;
+//     int8_t xPos;
+//     int8_t yPos;
 
-    public:
-    Player();
-    Player(int, int);
-    void moveUp();
-    void moveDown();
-    void moveLeft();
-    void moveRight();    
-}
+//     public:
+//     Player();
+//     Player(int, int);
+//     void moveUp();
+//     void moveDown();
+//     void moveLeft();
+//     void moveRight();    
+// }
