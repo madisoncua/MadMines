ee72c07e27363332b51cbd90faaf6e23d32a8661
Madison Cua <madison.cua@gmail.com>
2025-04-10 19:12:20 -0500

    Update Player.h Fixed file

--- Player.h
+++ Player.h
@@ -1,18 +1,22 @@
-// #include <iostream>
-// using namespace std;
-// class Player{
-//     friend class machine; //machines can access
-//     friend class Lab9HMain;
-//     private:
-//     int possession;
-//     int8_t xPos;
-//     int8_t yPos;
+ #ifndef _Player_h
+ #define _Player_h
+ #include <iostream>
+ using namespace std;
+ class Player{
+     friend class machine; //machines can access
+     friend class Lab9HMain;
 
-//     public:
-//     Player();
-//     Player(int, int);
-//     void moveUp();
-//     void moveDown();
-//     void moveLeft();
-//     void moveRight();    
-// }
+     private:
+     int possession;
+     uint8_t xPos;
+     uint8_t yPos;
+
+     public:
+     Player();
+     Player(uint8_t, uint8_t);
+     void moveUp(void);
+     void moveDown(void);
+     void moveLeft(void);
+     void moveRight(void);    
+ };
+ #endif
