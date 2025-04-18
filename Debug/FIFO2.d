49efe71ee29d984f2a36ecd8a9e5690220c32760
Madison Cua <madison.cua@gmail.com>
2025-04-17 20:25:05 -0500

    Fixed counters and smelter

---
+++ Debug/FIFO2.d
@@ -0,0 +1,5 @@
+# FIXED
+
+FIFO2.o: ../FIFO2.cpp ../../inc/FIFO2.h ../../inc/ST7735.h
+../../inc/FIFO2.h:
+../../inc/ST7735.h:
---
+++ Debug/Player.d
@@ -0,0 +1,6 @@
+# FIXED
+
+Player.o: ../Player.cpp ../Player.h ../Machine.h ../../inc/ST7735.h
+../Player.h:
+../Machine.h:
+../../inc/ST7735.h:
