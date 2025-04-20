313fd63eb5fd7c99ee99c68c9a4b0fbd76be3202
Madison Cua <madison.cua@gmail.com>
2025-04-20 14:38:55 -0500

    Updated sound and small visual fixes

--- Debug/Player.d
+++
@@ -1,6 +0,0 @@
-# FIXED
-
-Player.o: ../Player.cpp ../Player.h ../Machine.h ../../inc/ST7735.h
-../Player.h:
-../Machine.h:
-../../inc/ST7735.h:
--- Debug/SmallFont.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-SmallFont.o: ../SmallFont.cpp \
- C:/Users/madis/OneDrive\ -\ The\ University\ of\ Texas\ at\ Austin/Documents/UT\ -\ School/24-25/319H/MSPM0_ValvanoWare/MadMines/SmallFont.h \
- ../../inc/ST7735.h
-C:/Users/madis/OneDrive\ -\ The\ University\ of\ Texas\ at\ Austin/Documents/UT\ -\ School/24-25/319H/MSPM0_ValvanoWare/MadMines/SmallFont.h:
-../../inc/ST7735.h:
